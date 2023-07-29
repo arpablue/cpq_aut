c8b73d12d95294be26ae1302a998047173c16610
arpablue <eng.aug.flores@gmail.com>
2023-07-28 21:45:57 -0400

    The Quote Lines CRUD methods has been uopdated.

---
+++ src/api/DataAPI/DataAPI.py
@@ -0,0 +1,68 @@
+#from robot.libraris.BuiltIn import BuiltIn
+from robot.api.deco import library
+from robot.api.deco import keyword
+from robot.api import logger
+import sys
+import os
+
+dir = os.path.dirname( __file__ )
+src_pos = dir.index('src')
+src_path = dir[:src_pos] + os.path.join('src','obj','BaseObj')
+sys.path.append( src_path )
+
+from BaseObj import BaseObj
+
+###
+# It is extencion of the methods used to manage the data of a Data object to apply commons methods.
+###
+@library
+class DataAPI_ext:
+    # It create a Quote object using as base a dictionary.
+    # -param dict(Dictionary): It is the data used to crearte the Quote object.
+    # -return( QuoteObj ) It is the Quote created from the data.
+    ###
+    @keyword
+    def create_from_data(self, dict ):
+        raise Exception( "It is necesary to create a create_from_data() method for " + self.__class__.__name__ )
+    ###
+    # It is a flexible comparison. Both objects could have differents numbers of atributes,
+    # but all attributes of the first object should exists in the second object and they 
+    # they should have the same values.
+    # -param objA(DataObj) : It is the first quotes to be compared.
+    # -param objB(DataObj) :It is the second Quote to be compared.
+    ###
+    @keyword
+    def compare(self, objA , objB):
+        if ( objA == None ) and ( objB == None ):
+            return True
+        if objA == None :
+            return False
+        return objA.compare_to( objB )
+    ###
+    # It is a flexible comparison. Both objects could have differents numbers of atributes,
+    # but all attributes of the first object should exists in the second object and they 
+    # they should have the same values.
+    # -param objA(DataObj) : It is the first quotes to be compared.
+    # -param objB(DataObj) :It is the second Quote to be compared.
+    ###
+    @keyword
+    def compare_attrs(self, objA , objB):
+        if ( objA == None ) and ( objB == None ):
+            return True
+        if objA == None :
+            return False
+        return objA.compare_attrs( objB )
+###
+    # It is a strict comparison. Both objects should have the same number of attributes,
+    # the same attributes and the same values.
+    # -param objA(DataObj) : It is the first quotes to be compared.
+    # -param objB(DataObj) :It is the second Quote to be compared.
+    ###
+    @keyword
+    def equal(self, objA , objB):
+        if ( objA == None ) and ( objB == None ):
+            return True
+        if objA == None :
+            return False
+        return objA.equal( objB )
+        
